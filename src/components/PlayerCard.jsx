450a9be592a78db60e78de9afcf2d2723b8802b8
Ryan Vreeke <rvreeke@michiganlabs.com>
2024-12-23 13:50:25 -0800

    fix: Fixed the displaying of a user's win percentage.

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -9,7 +9,9 @@ function PlayerCard({ _id, name, wins, losses, showDetails }) {
 				<hr />
 				<p className="player-stats">Wins: {wins}</p>
 				<p className="player-stats">Losses: {losses}</p>
-				<p className="player-stats">Win Ratio: {wins / losses}</p>
+				<p className="player-stats">
+					Win Ratio: {((wins / (wins + losses)) * 100).toFixed(2)}%
+				</p>
 				<Link to={`/players/${_id}`}>Details</Link>
 			</div>
 		);
@@ -20,7 +22,9 @@ function PlayerCard({ _id, name, wins, losses, showDetails }) {
 				<hr />
 				<p className="player-stats">Wins: {wins}</p>
 				<p className="player-stats">Losses: {losses}</p>
-				<p className="player-stats">Win Ratio: {wins / losses}</p>
+				<p className="player-stats">
+					Win Ratio: {((wins / (wins + losses)) * 100).toFixed(2)}%
+				</p>
 			</div>
 		);
 	}
